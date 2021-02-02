fbb6763e9c3a96bbd57de3692750c23516abeba8
Vinayak Gupta <guptavinayakg@gmail.com>
2021-02-02 20:46:39 +0530

    Basic Change

--- assets/sketch.js
+++ assets/sketch.js
@@ -7,32 +7,15 @@ var shipImage, bulletImage, particleImage;
 var MARGIN = 40;
 var score = 0;
 var life = 3;
-var remov = true;
 
 function setup() {
     createCanvas(800, 600);
-
     bulletImage = loadImage('./assets/asteroids_bullet.png');
     shipImage = loadImage('./assets/asteroids_ship0001.png');
     particleImage = loadImage('./assets/asteroids_particle.png');
-
-    ship = createSprite(width / 2, height / 2);
-    ship.maxSpeed = 60;
-    ship.friction = 0.7;
-    ship.setCollider('circle', 0, 0, 20);
-
-    ship.addImage('normal', shipImage);
-    ship.addAnimation('thrust', './assets/asteroids_ship0002.png', './assets/asteroids_ship0007.png');
-
     asteroids = new Group();
     bullets = new Group();
-
-    for (var i = 0; i < 8; i++) {
-        var ang = random(360);
-        var px = width / 2 + 1000 * cos(radians(ang));
-        var py = height / 2 + 1000 * sin(radians(ang));
-        createAsteroid(3, px, py);
-    }
+    reset();
 }
 
 function draw() {
@@ -41,8 +24,9 @@ function draw() {
     textAlign(CENTER);
     textSize(16);
     text('Controls: Arrow Keys + X', width / 2, 20);
+    text(`Score: ${score}`, 4 * width / 5, 30);
+    if (life < 2) fill(255, 0, 0)
     text(`Lives: ${life}`, width / 5, 30);
-    text(`Score: ${score}`, 4 * width / 4, 30);
     for (var i = 0; i < allSprites.length; i++) {
         var s = allSprites[i];
         if (s.position.x < -MARGIN) s.position.x = width + MARGIN;
@@ -51,17 +35,11 @@ function draw() {
         if (s.position.y > height + MARGIN) s.position.y = -MARGIN;
     }
     asteroids.overlap(bullets, asteroidHit);
-    ship.bounce(asteroids, decLife);
+    ship.overlap(asteroids, decLife);
     if (keyDown(LEFT_ARROW))
-        ship.rotation -= 4;
+        ship.addSpeed(20, 180);
     if (keyDown(RIGHT_ARROW))
-        ship.rotation += 4;
-    if (keyDown(UP_ARROW)) {
-        ship.addSpeed(20, ship.rotation);
-        ship.changeAnimation('thrust');
-    }
-    else
-        ship.changeAnimation('normal');
+        ship.addSpeed(20, 0);
     if (keyWentDown('x')) {
         var bullet = createSprite(ship.position.x, ship.position.y);
         bullet.addImage(bulletImage);
@@ -69,22 +47,22 @@ function draw() {
         bullet.life = 20;
         bullets.add(bullet);
     }
-    if (life === 0) {
+    if (life <= 0) {
         for (var j = 0; j < allSprites.length; j++) {
             var t = allSprites[j];
             t.setSpeed(0);
         }
-        background(10);
-        textSize(48);
-        fill(255,0,0);
+        background(255);
+        textSize(width / 10);
+        fill(255, 0, 0);
         text("GAME OVER", width / 2, 170);
-        textSize(32);
+        textSize(width / 12);
         text(`Score: ${score}`, width / 2, 300);
-        text("Press 'R' to restart", width/2, 430);
+        text("Press 'R' to restart", width / 2, 430);
+        removeAll()
         if (keyWentDown('r')) {
             reset();
         }
-        if(remov) startRemove();
     }
     drawSprites();
 }
@@ -93,7 +71,7 @@ function createAsteroid(type, x, y) {
     var a = createSprite(x, y);
     var img = loadImage('./assets/asteroid' + floor(random(0, 3)) + '.png');
     a.addImage(img);
-    a.setSpeed(2.5 - (type / 2), random(360));
+    a.setSpeed(5 - (type / 2), random(85, 95));
     a.rotationSpeed = 0.5;
     a.type = type;
 
@@ -138,19 +116,25 @@ function decLife(ship, asteroid) {
 function reset() {
     score = 0;
     life = 3;
-    ship.position.x = 400;
-    ship.position.y = 300;
-    remov = true;
+    ship = createSprite(width / 2, height - 20);
+    ship.maxSpeed = 60;
+    ship.friction = 0.7;
+    ship.rotation = 270;
+    ship.setCollider('circle', 0, 0, 20);
+    ship.addImage(shipImage);
+    // ship.addImage('normal', shipImage);
+    // ship.addAnimation('thrust', './assets/asteroids_ship0002.png', './assets/asteroids_ship0007.png');
     asteroids.removeSprites();
     for (var i = 0; i < 8; i++) {
         var ang = random(360);
-        var px = width / 2 + 1000 * cos(radians(ang));
-        var py = height / 2 + 1000 * sin(radians(ang));
+        var px = width / 2 + 0.4 * width * cos(radians(ang));
+        var py = height / 2 + 0.4 * height * sin(radians(ang));
         createAsteroid(3, px, py);
     }
 }
 
-function startRemove() {
-    remov = false;
-    setInterval(() => {allSprites[0].remove()}, 2000);
+function removeAll() {
+    for (i = 0; i < allSprites.length;) {
+        allSprites[i].remove();
+    }
 }
